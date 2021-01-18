a2cfe15de09faeb478f11c75720dffc0afa0abae
Calvin Conley <cconley0125@gmail.com>
2021-01-17 20:58:04 -0500

    can add a todo using modal form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -55,8 +55,71 @@ let templateObject = {
   }
 };
 
-let mainTemplate = Handlebars.compile($('#main_template').html());
-$('[data-type="partial"]').each((_, partial) => {
-  Handlebars.registerPartial(`${partial.id}`, `${$(partial).html()}`);
-});
-$('body').append(mainTemplate(templateObject));
+class TodoList {
+  addFromForm() {
+    let $form = $('#form_modal');
+    let $userInputs = $form.find('select, input[type="text"], textarea');
+    let todo = this.makeTodoObjectFromInputs($userInputs);
+    $.ajax({
+      type: 'post',
+      url: '/api/todos',
+      data: JSON.stringify(todo),
+      contentType: 'application/json'
+    });
+  }
+
+  makeTodoObjectFromInputs($userInputs) {
+    let todo = {}
+    $userInputs.each((_, input) => {
+      if (input.value) {
+        todo[input.id] = input.value;
+      }
+    });
+    
+    return todo;
+  }
+}
+
+class App {
+  constructor() {
+    this.renderPage();
+    this.todoList = new TodoList();
+  }
+
+  renderPage() {
+    let mainTemplate = Handlebars.compile($('#main_template').html());
+
+    $('[data-type="partial"]').each((_, partial) => {
+      Handlebars.registerPartial(`${partial.id}`, `${$(partial).html()}`);
+    });
+
+    $('body').append(mainTemplate(templateObject));
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    $('label[for="new_item"').on('click', $.proxy(this.handleNewItemClick, this));
+    $('#modal_layer').on('click', $.proxy(this.handleModalLayerClick, this));
+    $('input[type="submit"').on('click', $.proxy(this.handleSaveClick, this));
+  }
+
+  handleNewItemClick() {
+    $('#form_modal').fadeIn();
+    $('#modal_layer').fadeIn();
+  }
+
+  handleModalLayerClick() {
+    $('#form_modal').fadeOut();
+    $('#modal_layer').fadeOut();
+  }
+
+  handleSaveClick(event) {
+    event.preventDefault();
+    this.todoList.addFromForm();
+    $('#form_modal').fadeOut();
+    $('#modal_layer').fadeOut();
+    $('#form_modal form')[0].reset();
+  }
+}
+
+$(() => new App());
